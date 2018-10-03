d3e3c2d7fed6b9516451d9386f898e7f667f6a48
adilsachwani <adilsachwani@gmail.com>
2018-10-03 18:39:06 +0500

    Splash Screen added

---
+++ app/src/main/java/com/example/scs/myapplication/SplashScreenActivity.java
@@ -0,0 +1,35 @@
+package com.example.scs.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    ImageView nedLogo;
+    Animation myAnim;
+    final int SPLASH_TIME = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        nedLogo = (ImageView) findViewById(R.id.ned_logo);
+        myAnim = AnimationUtils.loadAnimation(this,R.anim.splash_anim);
+        nedLogo.startAnimation(myAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME);
+    }
+}
